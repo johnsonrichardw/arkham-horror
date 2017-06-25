7c7b0f2eb5990d29afc665f54a991b617e2c9b96
Richard Johnson <johnsonrichardw@gmail.com>
2017-06-25 16:49:59 -0700

    Added 'Curse of the Rougarou' and 'Carnevale of Horrors' scenario's token pools

--- ah-chaos-bag.py
+++ ah-chaos-bag.py
@@ -5,8 +5,8 @@ import argparse
 
 #List of known token types, used later to validate tokens added by command line
 tokens = ['+1','0','-1','-2','-3','-4','-5','-6','-8','Skull','Cultist','Tablet','Elder Thing','Elder Sign','Tentacle']
-#List of supported campaigns
-campaigns = ['notz','tdl']
+#List of supported campaigns/scenarios
+campaigns = ['notz','tdl','coh','cotr']
 #List of supported difficulties
 difficulties = ['easy','standard','hard','expert']
 
@@ -29,19 +29,25 @@ token_sets['tdl-easy'] = ['+1','+1','0','0','0','-1','-1','-1','-2','-2','Skull'
 token_sets['tdl-standard'] = ['+1','0','0','-1','-1','-1','-2','-2','-3','-4','Skull','Skull','Cultist','Tentacle','Elder Sign']
 token_sets['tdl-hard'] = ['0','0','0','-1','-1','-2','-2','-3','-3','-4','-5','Skull','Skull','Cultist','Tentacle','Elder Sign']
 token_sets['tdl-expert'] = ['0','-1','-1','-2','-2','-3','-3','-4','-4','-5','-6','-8','Skull','Skull','Cultist','Tentacle','Elder Sign']
+#Carnevale of Horrors token pools
+token_sets['coh-standard'] = ['+1','0','0','0','-1','-1','-1','-2','-2','-3','-4','-6','Skull','Skull','Skull','Cultist','Tablet','Elder Thing','Tentacle','Elder Sign']
+token_sets['coh-hard'] = ['+1','0','0','0','-1','-1','-3','-4','-5','-6','-7','Skull','Skull','Skull','Cultist','Tablet','Elder Thing','Tentacle','Elder Sign']
+#Curse of the Rougarou
+token_sets['cotr-standard'] = ['+1','+1','0','0','0','-1','-1','-1','-2','-2','-3','-3','-4','-4','-5','-6','Skull','Skull','Cultist','Cultist','Tablet','Elder Thing','Tentacle','Elder Sign']
+token_sets['cotr-hard'] = ['+1','0','0','0','-1','-1','-1','-2','-2','-3','-3','-4','-4','-5','-5','-6','-8','Skull','Skull','Skull','Cultist','Cultist','Tablet','Elder Thing','Tentacle','Elder Sign']
 
 #Get token set to use
 set = args.campaign + '-' + args.difficulty
 if set not in token_sets.keys():
-	raise ValueError('Unknown token set: ' + set)
+    raise ValueError('Unknown token set: ' + set)
 token_list = token_sets[set]
 
 #Add specified new tokens to set
 if args.tokens != None:
-	for tok in args.tokens:
-		if tok not in tokens:
-			raise ValueError('Unknown token type: ' + tok)
-		token_list.append(tok)
+    for tok in args.tokens:
+        if tok not in tokens:
+            raise ValueError('Unknown token type: ' + tok)
+        token_list.append(tok)
 
 #Randomly choose a token from the list and print it out
 print(random.choice(token_list))
